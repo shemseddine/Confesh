ee3de51cb16cc0f9e51e3d2a07003d42ccad134a
Shems Eddine <shemseddine13@hotmail.co.uk>
2014-10-22 13:34:38 +0100

    Added the facades for HTML and Form
    closes #3

--- Confesh/config/app.php
+++ Confesh/config/app.php
@@ -127,6 +127,8 @@ return [
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
+		'Illuminate\Html\HtmlServiceProvider',
+	
 
 	],
 
@@ -185,6 +187,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'Form'  => 'Illuminate\Html\FormFacade',
+        'HTML'  => 'Illuminate\Html\HtmlFacade',
 
 	],
 
